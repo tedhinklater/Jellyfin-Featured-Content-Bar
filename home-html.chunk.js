bd845599b0fe61d4922d25a1d4d1f598507040e3
tedhinklater <66086488+tedhinklater@users.noreply.github.com>
2025-11-09 00:46:39 +0000

    Update home-html.chunk.js

--- home-html.chunk.js
+++ home-html.chunk.js
@@ -4,7 +4,7 @@
     e.default = `
     <div id="indexPage" style="outline:0" data-role="page" data-dom-cache="true" class="page homePage libraryPage allLibraryPage backdropPage pageWithAbsoluteTabs withTabs" data-backdroptype="movie,series,book">
       <style>
-        .featurediframe {width: 97.5vw; height: 23.5em; display: block; border: 0px solid #000; margin: 0 auto; margin-bottom: 0em; margin-top: 1em;}
+        .featurediframe {width: 97.5vw; height: 18.5em; display: block; border: 0px solid #000; margin: 0 auto; margin-bottom: 0em; margin-top: 1em;}
     @media (max-width: 1000px) and (orientation: portrait) {.featurediframe {height: 25em; margin-bottom: -8em;} }
     @media (max-width: 1000px) and (orientation: landscape) {.featurediframe {height: 26em; margin-bottom: -6em;} }
     @media (max-width: 421px) and (orientation: portrait) {.featurediframe {height: 25em; margin-bottom: -3em;} }
