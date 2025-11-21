30e3be5cd2e7a5eda7b0ff4c0b37016d11ac3b1e
tedhinklater <66086488+tedhinklater@users.noreply.github.com>
2025-11-21 03:43:53 +0000

    Adjust iframe height for smaller screens

--- home-html.chunk.js
+++ home-html.chunk.js
@@ -7,7 +7,7 @@
         .featurediframe {width: 97.5vw; height: 23.5em; display: block; border: 0px solid #000; margin: 0 auto; margin-bottom: 0em; margin-top: 1em;}
     @media (max-width: 1000px) and (orientation: portrait) {.featurediframe {height: 25em; margin-bottom: -8em;} }
     @media (max-width: 1000px) and (orientation: landscape) {.featurediframe {height: 26em; margin-bottom: -6em;} }
-    @media (max-width: 421px) and (orientation: portrait) {.featurediframe {height: 25em; margin-bottom: -3em;} }
+    @media (max-width: 421px) and (orientation: portrait) {.featurediframe {height: 20em; margin-bottom: -7em;} }
     @media (max-height: 421px) and (orientation: landscape) {.featurediframe {height: 83vh; margin-top: -1em;} }
       </style>
       <div class="tabContent pageTabContent" id="homeTab" data-index="0"><iframe class="featurediframe" src="/web/ui/spotlight.html"></iframe><div class="sections"></div></div><div class="tabContent pageTabContent" id="favoritesTab" data-index="1"> <div class="sections"></div></div></div>`;}}]);
